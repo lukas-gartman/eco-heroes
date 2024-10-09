b9a7c18d1e078b5aae6b056d1d7c2ca09b1e0d87
lukas-gartman <gusgartlu@student.gu.se>
2024-10-09 09:27:11 +0200

    remove test player

--- lib/main.dart
+++ lib/main.dart
@@ -17,28 +17,6 @@ class EcoHeroes extends StatelessWidget {
           directional: JoystickDirectional(),
         )
       ],
-      player: SimplePlayer(
-        position: Vector2(100, 100),
-        size: Vector2(32, 32),
-        animation: SimpleDirectionAnimation(
-          idleRight: SpriteAnimation.load(
-            'player_idle_right.png',
-            SpriteAnimationData.sequenced(
-              amount: 1,
-              stepTime: 0.1,
-              textureSize: Vector2(32, 32),
-            ),
-          ),
-          runRight: SpriteAnimation.load(
-            'player_run_right.png',
-            SpriteAnimationData.sequenced(
-              amount: 4,
-              stepTime: 0.1,
-              textureSize: Vector2(32, 32),
-            ),
-          ),
-        ),
-      ),
       map: WorldMapByTiled(
         WorldMapReader.fromAsset('eco-heroes.tmj'),
       ),

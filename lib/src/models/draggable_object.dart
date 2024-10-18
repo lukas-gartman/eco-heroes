b519006fe9ec18eb740bb5c7c0352921d297d630
Hampus Lyrstrand <Hampus Lyrstrand>
2024-10-18 16:50:53 +0200

    Added some draggable logic

--- lib/src/models/draggable_object.dart
+++ lib/src/models/draggable_object.dart
@@ -1,9 +1,36 @@
-import 'game_object.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flutter/material.dart';
 
-abstract class DraggableObject extends GameObject {
-  DraggableObject(super.pos, super.size);
-  
-  void onStartDrag();
-  void onDragUpdate();
-  void onEndDrag();
+class DraggableObject extends PositionComponent with DragCallbacks {
+  DraggableObject(Vector2 position, Vector2 size) {
+    this.position = position;
+    this.size = size;
+  }
+
+  final Paint _paint = Paint();
+
+  @override
+  void render(Canvas canvas) {
+    // Render a rectangle that changes color when dragged
+    _paint.color = isDragged ? Colors.red : Colors.white;
+    canvas.drawRect(size.toRect(), _paint);
+  }
+
+  @override
+  void onDragStart(DragStartEvent event) {
+    super.onDragStart(event);
+    // Optionally, handle logic when drag starts
+  }
+
+  @override
+  void onDragUpdate(DragUpdateEvent event) {
+    position += event.localDelta; // Update position as the object is dragged
+  }
+
+  @override
+  void onDragEnd(DragEndEvent event) {
+    super.onDragEnd(event);
+    // Optionally, handle logic when drag ends
+  }
 }

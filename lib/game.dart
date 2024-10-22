b63911b740970cced44f87c7e46effa703ca4734
Hampus Lyrstrand <Hampus Lyrstrand>
2024-10-22 10:37:40 +0200

    Added logic for dialogs

--- lib/game.dart
+++ lib/game.dart
@@ -5,49 +5,58 @@ import 'src/models/players/eco_hero_player.dart';
 import 'src/models/interactive_objects/trash.dart';
 import 'package:eco_heroes/src/models/mini_game.dart'; // Import the abstract class
 import 'package:eco_heroes/src/models/mini_games/trash_picking_mini_game.dart'; // Import the mini-game class
+import 'package:eco_heroes/src/models/dialog.dart'; // Import your GameDialog class
 
 class Game extends StatefulWidget {
   @override
   _GameState createState() => _GameState();
 }
-
 class _GameState extends State<Game> {
   final double tileSize = 16;
   final double mapWidth = 320; // Set according to your map's width
   final double mapHeight = 320; // Set according to your map's height
   final int numberOfTrashCans = 5;
 
-  // A single ValueNotifier to track the overall visibility of the interact button
   ValueNotifier<bool> showInteractButton = ValueNotifier(false);
   late TrashPickingMiniGame miniGame;
 
   @override
   void initState() {
     super.initState();
+    _showIntroDialog(); //kommentera ut den här för att slippa intro dialogen
     miniGame = TrashPickingMiniGame(
       numberOfTrashCans: numberOfTrashCans,
       mapWidth: mapWidth,
       mapHeight: mapHeight,
       tileSize: tileSize,
     );
-    miniGame.start(); // Start the mini-game and generate positions
+    miniGame.start(); 
+    // Start the mini-game and generate positions
   }
 
+  // Method to show introductory dialog
+  Future<void> _showIntroDialog() async {
+    await Future.delayed(Duration(milliseconds: 500)); // Small delay to ensure the screen loads
+    await GameDialog.introDialog(context); // Show the dialog
+  }
+  
   @override
   Widget build(BuildContext context) {
+    // Ensure trash positions are generated before accessing them
+    if (miniGame.trashPositions.isEmpty) {
+      return Center(child: CircularProgressIndicator());
+    }
+
+
+    // Track the current proximity state of each trash can
+    List<bool> proximityStates = List.filled(numberOfTrashCans, false);
+
     return LayoutBuilder(builder: (context, constraints) {
-      // Ensure trash positions are generated before accessing them
-      if (miniGame.trashPositions.isEmpty) {
-        return Center(child: CircularProgressIndicator());
-      }
-
-      // Track the current proximity state of each trash can
-      List<bool> proximityStates = List.filled(numberOfTrashCans, false);
-
       return Material(
         color: Colors.transparent,
         child: Stack(
           children: [
+
             BonfireWidget(
               playerControllers: [
                 Joystick(directional: JoystickDirectional()),
@@ -101,5 +110,6 @@ class _GameState extends State<Game> {
 
   void _onInteract() {
     print('Player interacted with a trash can!');
+    GameDialog.minigameDialog1(context);
   }
 }

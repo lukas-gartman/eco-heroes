cdf6906091a1e15e13d7b5be74d7cc4a98db4bc3
Hemlos <hannessvahn1999@gmail.com>
2024-10-25 22:50:23 +0200

    Fixed minigame
    Simply changed the sprites from trash to plants once they were interacted with. Simple game logic is now done.

--- lib/src/models/interactive_objects/hole.dart
+++ lib/src/models/interactive_objects/hole.dart
@@ -1,17 +1,22 @@
 import 'package:bonfire/bonfire.dart';
 import 'package:eco_heroes/src/models/enums/interaction_type.dart';
 import 'package:eco_heroes/src/models/interactive_object.dart';
+import 'package:flutter/widgets.dart';
 
 class Hole extends InteractiveObject {
   @override
   InteractionType get interactionType => InteractionType.clean;
 
   Hole({required super.position})
-    : super(size: Vector2(32, 32), sprite: Sprite.load('trash.png'));
+      : super(size: Vector2(32, 32), sprite: Sprite.load('trash.png'));
+
+  Future<void> changeSprite() async {
+    // Load the new sprite
+    sprite = await Sprite.load('planted_tree.png');
+  }
 
   @override
   void interact() {
-    print(this);
-    removeFromParent();
+    changeSprite(); // Replace with your new sprite path
   }
 }
--- lib/src/models/mini_games/chopped_forrest_mini_game.dart
+++ lib/src/models/mini_games/chopped_forrest_mini_game.dart
@@ -13,22 +13,21 @@ class ChoppedForrestMiniGame extends MiniGame {
   static const double tileSize = 16;
   static const double mapWidth = 640;
   static const double mapHeight = 640;
-  static const int numberOfHoles = 8;
+  static const int numberOfHoles = 8; //Not dynamic in this, if this is changed remember to change in method generateHoles();
 
   late List<Hole> holes;
   late List<SquirrelNPC> squirrels;
+
   List<InteractiveObject> combinedList = [];
   final double proximityRange = 40;
-  int collectedTrash = 0;
+  int plantedSeeds = 0;
   bool isStart = true;
   bool isCompleted = false;
 
   ChoppedForrestMiniGame(super.onCompleted);
 
-  
-
   @override
-  List<GameObject> get objects => combinedList;//Changed to combinedList from trashCans
+  List<GameObject> get objects => combinedList; //Changed to combinedList from trashCans
 
   @override
   GameMap get map => WorldMapByTiled(WorldMapReader.fromAsset('maps/chopped_down_forrest/choppedDownForrest.tmj'), forceTileSize: Vector2.all(tileSize));
@@ -36,18 +35,19 @@ class ChoppedForrestMiniGame extends MiniGame {
   @override
   void start() {
     squirrels = generateNPCs(); //Currently only creates one squirrel
-    holes = generateRandomTrashCans(); 
+    holes = generateHoles(); 
+    //plantedTrees = generateTrees();
     
+    //Create a combined list with trashcans and NPCs
     combinedList.addAll(squirrels);
     combinedList.addAll(holes);
     
-    //Create a combined list with trashcans and NPCs
     super.proximityChecker = ProximityChecker(
       objects: combinedList,
       proximityRange: proximityRange,
       inProximityWith: ValueNotifier(null), // Initialize the button state
     );
-    print("Trash picking mini-game started with positions: $holes");
+    //print("Trash picking mini-game started with positions: $holes");
   }
 
   @override
@@ -55,35 +55,39 @@ class ChoppedForrestMiniGame extends MiniGame {
     if (isCompleted) return;
     if (isStart) {
       isStart = false;
-      TalkDialog.show(context, GameDialog.trashPickingIntroDialog());
+      //Change intro dialogue 
+      TalkDialog.show(context, GameDialog.plantingIntroDialogue());
       return;
     }
     
     super.proximityChecker.checkProximity(playerPosition); // Call the proximity checker in each update with the player's position
-    if (holes.isEmpty) {
+    if (plantedSeeds == numberOfHoles) {
       isCompleted = true;
-      TalkDialog.show(context, GameDialog.trashPickingEndDialog(), onFinish: () => super.onCompleted());
+      TalkDialog.show(context, GameDialog.plantingEndDialog(), onFinish: () => super.onCompleted());
     }
   }
 
-  List<Hole> generateRandomTrashCans() {
+  List<Hole> generateHoles() {
 
     //To save time for submission, statically generate 8 positions of Holes
 
     final List<Hole> holeList = [];
 
     holeList.add(Hole(position: Vector2(71, 145)));
-    holeList.add(Hole(position: Vector2(184, 292)));
-    holeList.add(Hole(position: Vector2(312, 466)));
+    holeList.add(Hole(position: Vector2(184, 240)));
+    holeList.add(Hole(position: Vector2(112, 466)));
     holeList.add(Hole(position: Vector2(458, 132)));
     holeList.add(Hole(position: Vector2(501, 381)));
     holeList.add(Hole(position: Vector2(239, 534)));
-    holeList.add(Hole(position: Vector2(366, 289)));
+    holeList.add(Hole(position: Vector2(398, 231)));
     holeList.add(Hole(position: Vector2(555, 472)));
 
     return holeList;
   }
 
+  
+
+  //Currently only 1 squirrel
   List<SquirrelNPC> generateNPCs(){
     final List<SquirrelNPC> squirrelList = [];
     SquirrelNPC squirrel = SquirrelNPC(position: Vector2(100, 100));
@@ -96,23 +100,15 @@ class ChoppedForrestMiniGame extends MiniGame {
   void interactWithObject(BuildContext context, GameObject object)  {
     if (object is Hole) {
 
-      /*When interacting with a hole it should.
-      1. Remove the hole
-      2. Replace where the hole was with a tree / seed
-      3. Have some sort of count down until its done
-
-      */
       object.interact();
-      holes.remove(object);
-      collectedTrash++;
       proximityChecker.removeObject(object);
-      print('Removed trash can at position: ${object.position}');
+      plantedSeeds++;
+      
     }
     
     if (object is SquirrelNPC) {
       object.interact();
-      TalkDialog.show(context, GameDialog.trashPickingSquirrelInteract(numberOfHoles - collectedTrash));
-      //print("Hello, im a squirrel. Please help us clean up!");
+      TalkDialog.show(context, GameDialog.plantingSquirrelDialog(numberOfHoles - plantedSeeds));
     }
   }
 }
